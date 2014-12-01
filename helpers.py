f683dc1cd025795067a42b1d526dd556a9222e67
Jonathan Huang <jon.hc.huang@gmail.com>
2014-12-01 16:11:32 -0500

    Added cleanstr to helpers

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 __auth__ = 'jhh283'
 
 from balanced_set import balanced_set
+import string
 
 
 def BuildXY(filename):
@@ -9,14 +10,12 @@ def BuildXY(filename):
     for movie in listify:
         y.append(movie['year'])
         x.append(movie['summary'])
+        # x.append(clean_str(movie['summary']))
     return [x, y]
 
-    # tr_y, tr_x, te_y, te_x = [], [], [], []
-    # listTrain = list(balanced_set('train.txt'))
-    # listTest = list(balanced_set('test.txt'))
-    # for movie in listTrain:
-    #     tr_y.append(movie['year'])
-    #     tr_x.append(movie['summary'])
-    # for movie in listTest:
-    #     te_y.append(movie['year'])
-    #     te_x.append(movie['summary'])
+
+# def clean_str(instr):
+#     """
+#     Helper to return string with punctuation and capital letters removed.
+#     """
+#     return instr.lower().translate(None, string.punctuation)
